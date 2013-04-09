f23b36d75179997cb9ca7385c0337e223e9e4949
Isaac Wilder <imw@thefnf.org>
2013-04-08 20:28:52 -0500

    test application functioning

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #imports
+from __future__ import with_statement
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash
+from contextlib import closing
 
 #config
-DATABASE = 'tmp/gus.db'
+DATABASE = '/tmp/gus.db'
 DEBUG = True
 SECRET_KEY = 'devkey'
 USERNAME = 'admin'
@@ -12,6 +14,23 @@ PASSWORD = 'default'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	g.db.close()
 
 @app.route('/')
 def explore():
@@ -29,14 +48,25 @@ def add_entry():
 	flash('New node added')
 	return redirect(url_for('explore'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid Username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Password'
+		else:
+			session['logged_in'] = True
+			flash('Success')
+			return redirect(url_for('explore'))
+	return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+	session.pop('logging_in', None)
+	flash('Logged out')
+	return redirect(url_for('explore'))
+
 if __name__ == "__main__":
     app.run()
-
-
-@app.before_request
-def before_request():
-	g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-	g.db.close()
